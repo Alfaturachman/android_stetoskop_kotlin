3ef90d162e23c4d22406a2d591cb03ad56cfdfc8
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-02-08 17:13:51 +0700

    initial commit

---
+++ app/src/main/java/com/example/stetoskop/ui/profile/ProfileFragment.kt
@@ -0,0 +1,32 @@
+package com.example.stetoskop.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.stetoskop.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
